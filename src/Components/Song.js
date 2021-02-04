1e741f03662bf2a4c34a8d7c59715e64eefd9c6f
Samantha Solomon <sleahsolomon@gmail.com>
2021-02-04 15:33:52 -0500

    built basic song, list, and form component

---
+++ src/Components/Song.js
@@ -0,0 +1,15 @@
+import React from "react"
+import { Route } from "react-router-dom"
+
+const Song = (props) => {
+    return(
+        <div>
+            <p>Song Title</p>
+            <p>Artist Name</p>
+            <p>Length</p>
+            <p>Favorite heart</p>
+        </div>
+    )
+}
+
+export default Song
